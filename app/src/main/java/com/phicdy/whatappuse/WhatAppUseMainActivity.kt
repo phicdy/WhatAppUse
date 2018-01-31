5d8a479f9f725d02f4cbfaa9f0702059d9762a56
phicdy <phicdy@gmail.com>
2018-01-31 23:05:25 +0900

    Initial commit

---
+++ app/src/main/java/com/phicdy/whatappuse/WhatAppUseMainActivity.kt
@@ -0,0 +1,12 @@
+package com.phicdy.whatappuse
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class WhatAppUseMainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_what_app_use_main)
+    }
+}
